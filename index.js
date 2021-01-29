c304d396ba690ad86fdb01df1818e3d603391b58
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2021-01-29 10:18:40 +0530

    Command parsing

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
+const COMMAND_CHAR = "!";
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
 client.on("message", message => {
-    if(!message.content.startsWith("!")) return
+    if(!message.content.startsWith(COMMAND_CHAR)) return
     if(message.author.tag == client.user.tag) return
 
-    //TODO: process the message
+    //TODO: process the message and get the command and arguements
+    let originalMessage = message.content.split(COMMAND_CHAR)[1];
 
-    else message.reply("Yo! You called me?")
+    originalMessage = originalMessage.split(/\s+/);
+    let command = originalMessage[0];
+    originalMessage.splice(0,1);
+    let args = originalMessage
+    message.reply(`Yo! You called me with command ${command} and args ${args}`)
 })
 
 
